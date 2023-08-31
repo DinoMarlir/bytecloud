98c2f113c4658fa75379c2ba39613e1b78cc325e
HttpMarco <mirco.lindenau@gmx.de>
2023-08-31 17:02:58 +0200

    Add new modules

--- bytecloud-daemon/src/main/kotlin/net/bytemc/bytecloud/daemon/database/sql/SqlDatabase.kt
+++ bytecloud-daemon/src/main/kotlin/net/bytemc/bytecloud/daemon/database/sql/SqlDatabase.kt
@@ -1,8 +1,22 @@
 package net.bytemc.bytecloud.daemon.database.sql
 
 import net.bytemc.bytecloud.daemon.database.Database
+import java.sql.Connection
+
 
 abstract class SqlDatabase : Database {
 
+    init {
+        this.executeUpdate("CREATE TABLE IF NOT EXISTS bytecloud_groups(id VARCHAR(255) PRIMARY KEY);")
+    }
 
+    abstract fun getConnection() : Connection
+
+    fun executeUpdate(url: String) {
+        try {
+            this.getConnection().prepareStatement(url).use { preparedStatement -> preparedStatement.executeUpdate() }
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+    }
 }
