2e08ff73d520b0278237346e1856b36b47e4827a
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2023-09-11 18:57:32 +0200

    PlatformManager.kt: added root folder

--- daemon/src/main/kotlin/net/bytemc/bytecloud/daemon/platform/PlatformManager.kt
+++ daemon/src/main/kotlin/net/bytemc/bytecloud/daemon/platform/PlatformManager.kt
@@ -1,6 +1,15 @@
 package net.bytemc.bytecloud.daemon.platform
 
+import java.io.File
+
 class PlatformManager {
+
+    private val rootFolder = run {
+        val file = File("storage/platforms")
+
+        if (!file.exists()) file.mkdirs()
+        file
+    }
 
     val platforms = hashMapOf<String, Platform>(
 
